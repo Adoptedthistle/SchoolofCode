614dd9e5b6866435ab554d693d9ed4c0f3e3a86e
AdoptedThistle <sbarkle.1@googlemail.com>
2020-01-27 12:44:00 +0000

    Add tasks from W2D1

---
+++ RPS/RPS homework.js
@@ -0,0 +1,14 @@
+//Add an event listner in JS to RPS when Player wins
+//Add button in HTML
+//Add an event listener in JS to RPS when Player loses
+//Add button in HTML
+//Add an event listener in JS to RPS when Player draws
+//Add button in HTML
+
+var element=document.getElementById(Player wins!){
+    playerWins.addeventlistner(click,function);
+} if(){
+
+} else(){
+
+}
---
+++ media-queries/Playground/playground.js
@@ -0,0 +1,27 @@
+//console.log("hello");
+//li.textContent = "experiment";
+//console.log(li.textContent);
+//console.log(li.innerText);
+
+//let.p=document.querySelector("p")
+
+//p.textContent +="whatever else we want";
+//p.insertAdjacentText("afterend" jjllooo);
+
+//const button = document.querySelector("button");
+
+//function toggleColor() {
+//const p = document.queryselector("p");
+//p.classlist.toggle("red");
+//console.log(p.classList.contains("red"));
+//}
+
+//button.addEventListner("click", toggleColor);
+
+//document.addEventListener("load", function() {
+//console.log("I'm running");
+//const img = document.querySelector("img");
+//console.log(img.naturalWidth);
+//});
+
+document.body.innerHTML = "<p>NewPage</p>";
